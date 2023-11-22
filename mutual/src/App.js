5cc5da164e1387c8244e17dcc3fdba4c73118a59
Sindhu-Peddi <147684620+Sindhu-Peddi@users.noreply.github.com>
2023-11-22 19:43:43 +0530

    Add files via upload

---
+++ mutual/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import MutualFundPage from './MutualFundPage';
+
+
+
+function App() {
+  return (
+    <div className="App">
+     <MutualFundPage/>
+    </div>
+  );
+}
+
+export default App;
